fc7653195f31a90ac48707b6568ef6904d7a6c99
marvin <marvin.tomales01@gmail.com>
2025-09-28 19:22:40 +0800

    MainForm-Buttons-TabControl

--- Form3.cs
+++ Form3.cs
@@ -16,5 +16,10 @@ namespace MachineProblemHospitalRecordSystem
         {
             InitializeComponent();
         }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+
+        }
     }
 }
