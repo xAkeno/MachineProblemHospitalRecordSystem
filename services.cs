765c6a2627e8b5192c948683d62033ae2175183f
xAkeno <clarkkentraguhos@gmail.com>
2025-09-29 10:40:20 +0800

    added conntion to db

---
+++ services.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MySql.Data.MySqlClient;
+
+namespace MachineProblemHospitalRecordSystem
+{
+    internal class services
+    {
+        protected static String connect = "Server=localhost; Database=hospital_records_db; Uid=root; Pwd=;";
+
+        protected MySqlConnection GetConnection()
+        {
+            return new MySqlConnection(connect);
+        }
+    }
+}
